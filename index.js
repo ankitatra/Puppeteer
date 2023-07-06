c011360a643fe46e580726600b91fea7951cb725
Ankita Thakur <ankitathakurankita7007@gmail.com>
2023-07-06 20:54:42 +0530

    add screenshot and read the data of website

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //npm i puppeteer
 
 const puppeteer=require("puppeteer")
-
-
+const fs=require("fs/promises")
+/*
 const startBrowser=async()=>{
     const browser=await puppeteer.launch({headless:"new"})
     const page=await browser.newPage()
@@ -11,5 +11,19 @@ const startBrowser=async()=>{
     await page.screenshot({path:"screenshot.png",fullPage:true})
     await browser.close()
 }
+*/
 
+const startBrowser=async()=>{
+    const browser=await puppeteer.launch({headless:"new"})
+    const page=await browser.newPage()
+    
+    await page.goto("https://example.cypress.io/todo")
+   const todos=await page.evaluate(()=>{
+   return Array.from( document.querySelectorAll(".view label")).map(el=>el.textContent)
+   })
+   console.log(todos)
+
+   await fs.writeFile("todos.txt",todos.join("\n"));
+   await browser.close();
+}
 startBrowser()
