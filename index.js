aebf72ad67539106815081ecf414fcf6826020aa
Ankita Thakur <ankitathakurankita7007@gmail.com>
2023-07-14 19:56:22 +0530

    add read file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //npm i puppeteer
 
-const puppeteer=require("puppeteer")
-const fs=require("fs/promises")
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 /*
 const startBrowser=async()=>{
     const browser=await puppeteer.launch({headless:"new"})
@@ -11,11 +11,12 @@ const startBrowser=async()=>{
     await page.screenshot({path:"screenshot.png",fullPage:true})
     await browser.close()
 }
-*/
+
+
 
 const startBrowser=async()=>{
     const browser=await puppeteer.launch({headless:"new"})
-    const page=await browser.newPage()
+    const page=await brodwser.newPage()
     
     await page.goto("https://example.cypress.io/todo")
    const todos=await page.evaluate(()=>{
@@ -26,4 +27,108 @@ const startBrowser=async()=>{
    await fs.writeFile("todos.txt",todos.join("\n"));
    await browser.close();
 }
-startBrowser()
+
+const startBrowser = async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto("https://unsplash.com/");
+  const images = await page.$$eval(".MorZF img", (el) => {
+    return el.map((el) => el.src);
+  });
+  console.log(images);
+//   for (let i = 0; i < images.length; i++) {
+//     const obj = {
+//       id: i + 1,
+//       image: images[i],
+//     };
+//     fs.writeFile(
+//       "images.json",
+//       `\n${JSON.stringify(obj)}`,
+//       { flag: "a+" },
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       }
+//     );
+//   }
+ let index=0
+  for(let img of images){
+    const realImage=await page.goto(img)
+
+    fs.writeFile(`images/images${++index}.jpeg`,
+    await realImage.buffer(),
+    {flag:"a+"},(err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+  }
+  await browser.close();
+};
+
+const startBrowser = async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto("https://unsplash.com/");
+
+  const links=await page.$$(".p7aj0")
+
+  for(let link of links){
+    const linkText=await link.evaluate((el)=>el.textContent)
+    if(linkText.trim()==="Animals"){
+      await link.click();
+      await new Promise((r)=>setTimeout(r,2000))
+      break;
+    }
+  }
+  const images = await page.$$eval(".MorZF img", (el) => {
+    return el.map((el) => el.src);
+  });
+  console.log(images);
+ let index=0
+  for(let img of images){
+    const realImage=await page.goto(img)
+
+    fs.writeFile(`images/images${++index}.jpeg`,
+    await realImage.buffer(),
+    {flag:"a+"},(err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+  }
+  await browser.close();
+};*/
+
+
+const startBrowser = async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto("https://unsplash.com/");
+
+   const searchInput=await page.$('input[type="search"]');
+
+   await searchInput.type("cars")
+   await searchInput.press("Enter")
+
+  const images = await page.$$eval(".MorZF img", (el) => {
+    return el.map((el) => el.src);
+  });
+  console.log(images);
+ let index=0
+  for(let img of images){
+    const realImage=await page.goto(img)
+
+    fs.writeFile(`images/images${++index}.jpeg`,
+    await realImage.buffer(),
+    {flag:"a+"},(err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+  }
+  await browser.close();
+};
+
+startBrowser();
